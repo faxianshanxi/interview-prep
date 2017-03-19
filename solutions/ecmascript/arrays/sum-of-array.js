2016df704b4e37639176ebc0398d190b6ca33ac4
Faxian Shanxi <faxian.shanxi@gmail.com>
2017-03-19 17:57:46 +0530

    Sum of array, conditional too - ecmascript

--- solutions/ecmascript/arrays/sum-of-array.js
+++ solutions/ecmascript/arrays/sum-of-array.js
@@ -1,9 +1,17 @@
 'use strict';
 
-function sumOfArray(array) {
-    return array.reduce((prev, curr) => prev + curr, 0);
+class SumOfArray {
+    constructor(array) {
+        this.array = array;
+    }
+    sum() {
+        return this.array.reduce((p, c) => p + c, 0);
+    }
+    conditionalSum(condition) {
+        return this.array.reduce((p, c, i, a) => condition(c, p, i, a) ? p + c : p, 0);
+    }
 }
 
 if(module) {
-    module.exports = sumOfArray;
+    module.exports = SumOfArray;
 }
--- solutions/ecmascript/arrays/sum-of-array.test.js
+++ solutions/ecmascript/arrays/sum-of-array.test.js
@@ -1,7 +1,16 @@
-const sumOfArray = require('./sum-of-array');
+const SumOfArray = require('./sum-of-array');
 
 function runTest(test) {
-    const actual = sumOfArray(test.input);
+    var soa = new SumOfArray(test.input);
+    const actual = soa.sum();
+    const expected = test.output;
+    console.log(`actual: ${actual}, expected: ${expected}`);
+    console.log(`actual === expected: ${actual === expected}`);
+}
+
+function runConditionalTest(test) {
+    var soa = new SumOfArray(test.input);
+    const actual = soa.conditionalSum(test.condition);
     const expected = test.output;
     console.log(`actual: ${actual}, expected: ${expected}`);
     console.log(`actual === expected: ${actual === expected}`);
@@ -13,7 +22,22 @@ function runTest(test) {
         { input: [], output: 0 },
         { input: [1, 2, 3], output: 6 },
         { input: [-1, 0, 1], output: 0 },
-        { input: [-1, -2, -3], output: 6 }
+        { input: [-1, -2, -3], output: -6 }
     ];
     testCases.forEach(runTest);
+    const conditionalCases = [
+        { input: [0], condition: x => !x, output: 0 },
+        { input: [], condition: x => !x, output: 0 },
+        { input: [1, 2, 3], condition: x => !x, output: 0 },
+        { input: [-1, 0, 1], condition: x => !x, output: 0 },
+        { input: [-1, -2, -3], condition: x => !x, output: 0 },
+        { input: [0], condition: x => !!x, output: 0 },
+        { input: [], condition: x => !!x, output: 0 },
+        { input: [1, 2, 3], condition: x => !!x, output: 6 },
+        { input: [-1, 0, 1], condition: x => !!x, output: 0 },
+        { input: [-1, -2, -3], condition: x => !!x, output: -6 },
+        { input: [1, 2, 3], condition: (x, y) => x + y <= 5, output: 3 },
+        { input: [-1, -2, -3], condition: (x, y) => x + y >= -5, output: -3 }
+    ];
+    conditionalCases.forEach(runConditionalTest);
 })();
